a69fa480331370acad10178599f3a6f42a98cd2b
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-08-27 16:52:40 -0300

    grafico de colunas com d3js usando fgv

--- script.js
+++ script.js
@@ -3,35 +3,45 @@ let svg = d3.select('#grafico');
 let largura = parseInt(svg.style('width'));
 let altura = parseInt(svg.style('height'));
 
+let larguraPlotagem = largura-40
+let alturaPlotagem = altura-40
+
+let plotagem = svg.append('g')
+    .attr('width',larguraPlotagem)
+    .attr('height',alturaPlotagem)
+    .attr('transform', 'translate(20, 20)')
+
+
 // Escala para o eixo X (largura das barras)
 // O domínio vai de 0 até o valor máximo nos dados
 // O alcance vai de 0 até a largura do SVG
-let fnx = d3.scaleLinear().domain([0, d3.max(dados)]).range([0, largura]);
+let fnx = d3.scaleLinear()
+    .domain([0, dados.length])
+    .range([0, larguraPlotagem]);
 
 // Escala para o eixo Y (posição e altura das barras)
 // O domínio vai de 0 até o número de dados
 // O alcance vai de 0 até a altura do SVG
-let fny = d3.scaleBand()
-    .domain(d3.range(dados.length))
-    .range([0, altura])
-    .padding(0.1); // Adiciona um pequeno espaçamento entre as barras
+let fny = d3.scaleLinear()
+    .domain([0,d3.max(dados)])
+    .range([alturaPlotagem,0])
 
-svg.selectAll('.barra')
+plotagem.selectAll('.coluna')
     .data(dados)
     .join('rect')
-    .attr('class', 'barra')
-    .attr('x', 0)
-    .attr('y', (d, i) => fny(i))
-    .attr('width', d => fnx(d))
-    .attr('height', fny.bandwidth());
-svg.selectAll('.rotulo')
+    .attr('class', 'coluna')
+    .attr('x', (d, i) => fnx(i))
+    .attr('y', (d) => fny(d))
+    .attr('width', fnx(1) * 0.9) 
+    .attr('height', (d) => alturaPlotagem - fny(d));
+plotagem.selectAll('.rotulo')
     .data(dados)
     .enter()
     .append('text')
     .attr('class', 'rotulo')
     .text((d)=>d)
-    .attr('x', (d)=>fnx(d))
-    .attr('dx', -5)
-    .attr('y', (d,i) => fny(i))
-    .attr('dy',(d,i) => fny(1)*0.5 );
+    .attr('x', (d,i)=>fnx(i))
+    .attr('dx', ()=>fnx(1)*0.5)
+    .attr('y', (d) => fny(d))
+    .attr('dy',-5);
     
